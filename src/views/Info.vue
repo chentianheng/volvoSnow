d5a92ce06aed2418dc01ebefb443ea9d4f494465
stenry <chentianhengg@gmail.com>
2019-07-30 10:46:47 +0800

    first commit

---
+++ src/views/Info.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="bgContainer" @click="toInfo">
+        <img class="title animated fadeInUp" src="../assets/img/title02.png" alt="" >
+        <img class="animated bounceIn delay-1s" src="../assets/img/roller.png" alt="" style="width: 80%;margin-top: 2rem">
+        <div style="position: relative;width: 70%">
+            <img class="animated fadeIn delay-2s" src="../assets/img/selectSkis.png" alt="" style="width: 100%;margin-top: 1rem">
+            <img class="animated bounceIn" v-show="isClick" src="../assets/icon/click.png" alt="" style="position: absolute;width: 28px;left: 10%;top: 40%">
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Equipment03",
+        data(){
+            return{
+                isClick: false
+            }
+        },
+        methods:{
+            toInfo(){
+                let that = this;
+                that.isClick = true;
+                setTimeout(function () {
+                    that.$router.push('/equipment03')
+                },1000)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .bgContainer{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background: url("../assets/bg/bg05.png") repeat center top;
+        background-size: auto 100% ;
+        position: relative;
+        overflow-x: hidden;
+        overflow-y: hidden;
+    }
+
+    .title{
+        margin-top: 2rem;
+        width: 60%;
+    }
+
+
+    @media only screen and (max-width: 320px){
+        .title{
+            margin-top: 1rem;
+            width: 60%;
+        }
+    }
+</style>
---
+++ src/views/Information.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="bgContainer">
+        <img src="../assets/icon/logo.png" alt="" style="width: 45px;height: 45px;position: absolute;top: 10px;right: 10px">
+        <img class="title animated fadeInUp" src="../assets/img/title.png" alt="">
+        <img class="btn animated bounceIn delay-2s" src="../assets/img/button01.png" alt="" @click="toItem">
+        <img class="whiteCar animated fadeInLeft delay-1s" src="../assets/img/white.png" alt="">
+        <img class="redCar animated fadeInRight" src="../assets/img/red.png" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        methods:{
+            toItem(){
+                this.$router.push('/item')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .bgContainer{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background: url("../assets/bg/bg02.png") repeat center top;
+        background-size: auto 100% ;
+        position: relative;
+        overflow-x: hidden;
+        overflow-y: hidden;
+    }
+
+    .title{
+        margin-top: 5rem;
+        width: 60%;
+    }
+
+    .btn {
+        margin-top: 15%;
+        width: 80%;
+    }
+
+    .whiteCar {
+        position: absolute;
+        left: 20px;
+        bottom: 3.5rem;
+        width: 26%;
+    }
+
+    .redCar {
+        position: absolute;
+        right: 20px;
+        bottom: 1rem;
+        width: 60%;
+    }
+</style>
